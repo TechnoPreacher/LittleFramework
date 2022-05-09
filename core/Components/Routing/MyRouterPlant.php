5017b0c49da047fe2382b33865ad297d4e86c161
TechnoPreacher <nik.nik.sulima@gmail.com>
2022-05-10 01:05:02 +0300

    router in factory

--- core/Components/Routing/MyRouterPlant.php
+++ core/Components/Routing/MyRouterPlant.php
@@ -11,8 +11,5 @@ class MyRouterPlant extends ComponentFactory
     protected function createConcreteComponent()
     {
         return new RouterAlias($this->arguments);
-//        return new RouterAlias('/c/d', function () {
-//            echo 'Run callback'; return 10;
-//        });
     }
 }
